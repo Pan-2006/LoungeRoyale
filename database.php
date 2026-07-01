4e17c216a86dacd7c59cf1d099dc24c0b3671b19
Pan-2006 <leonardvincentcamat@gmail.com>
2026-07-01 15:32:53 +0800

    Update database.php

--- database.php
+++ database.php
@@ -1,21 +1,26 @@
 <?php
-$host = "127.0.0.1";
-$user = "root";
-$password = "";
-$db = "lounge_royale";
-$ports = [3306, 3307];
+mysqli_report(MYSQLI_REPORT_OFF);
 
-$conn = null;
+$isLocal = in_array($_SERVER['SERVER_NAME'] ?? '', ['localhost', '127.0.0.1']);
 
-foreach ($ports as $port) {
-    $conn = @mysqli_connect($host, $user, $password, $db, $port);
-    if ($conn) {
-        break;
-    }
+if ($isLocal) {
+    $host = "127.0.0.1";
+    $user = "root";
+    $password = "";
+    $db = "lounge_royale";
+    $port = 3306;
+} else {
+    $host = "YOUR_ONLINE_DB_HOST";
+    $user = "YOUR_ONLINE_DB_USERNAME";
+    $password = "YOUR_ONLINE_DB_PASSWORD";
+    $db = "YOUR_ONLINE_DB_NAME";
+    $port = 3306;
 }
 
+$conn = mysqli_connect($host, $user, $password, $db, $port);
+
 if (!$conn) {
-    die("Database connection failed: " . mysqli_connect_error());
+    die("Database connection failed.");
 }
 
 mysqli_set_charset($conn, "utf8mb4");
